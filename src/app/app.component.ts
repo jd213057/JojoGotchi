04613f2ce55efc16d09a9e50c1990bc183f0e44d
jd213057 <you@example.com>
2020-07-15 23:04:35 +0100

    création du projet + intégartion cordova + installation local notif plugin + debut mise en page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JojoGotchi } from './classes/JojoGotchi';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'JojoGotchi';
+  jojoGotchi: JojoGotchi;
+
+  constructor() {}
+
+  getHealth(): number {
+ return this.jojoGotchi.getHealth();
+  }
+
+  setEatNotification() {
+
+  }
 }
---
+++ src/app/classes/JojoGotchi.ts
@@ -0,0 +1,45 @@
+export class JojoGotchi {
+
+  name: string;
+  birthday: string;
+  age: number;
+  health: number;
+
+  constructor(name) {
+this.name = name;
+this.initBirthday();
+this.age = 0;
+const today = new Date();
+this.birthday = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  }
+
+  initBirthday(): void {
+  const today = new Date();
+  this.birthday = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  }
+
+  getBirthDay(): string {
+    return this.birthday;
+      }
+
+      getName(): string {
+        return this.name;
+          }
+
+  getAge(): number {
+return this.age;
+  }
+
+  setAge(age: number): void {
+    this.age = age;
+  }
+
+  getHealth(): number {
+    return this.health;
+      }
+
+      setHealth(age: number): void {
+        this.age = this.health;
+      }
+
+}
